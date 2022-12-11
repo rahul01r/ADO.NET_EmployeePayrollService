292c3cc204d9c2d487ca99fccc036584bc143dc3
rahul01r <rrdubeynibm@gmail.com>
2022-12-11 21:28:02 +0530

    [rahul] Add:UC1-ADO.NET EmployeePayrollServie

---
+++ ADO.NET_PayrollService/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ADO.NET_PayrollService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To SQL Program");
+            EmployeeRepository.GetAllEmployee();
+            Console.ReadLine();
+        }
+    }
+}
